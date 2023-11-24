04ab03dd52d21217c48cd24f58dedf6f72cc7934
quangto188 <todiuquang123@gmail.com>
2023-11-24 07:45:40 +0700

    update

--- source_code/Core/Inc/global.h
+++ source_code/Core/Inc/global.h
@@ -8,20 +8,52 @@
 #ifndef INC_GLOBAL_H_
 #define INC_GLOBAL_H_
 
-#define RED_TIME			500
-#define GREEN_TIME			300
-#define	YELLOW_TIME			200
+// declare the duration of all default leds 
+#define RED_TIME							500
+#define GREEN_TIME							300
+#define	YELLOW_TIME							200
 
-#define button_mode_is_pressed		2
-#define button_add_is_pressed		3
-#define button_confirm_is_pressed	4
-#define button_reset_is_pressed		5
+#define NORMAL_MODE							1
+#define RED_MODE							2
+#define AMBER_MODE							3
+#define GREEN_MODE							4
+#define MAN_RED_MODE						5
+#define MAN_AMBER_MODE						6
+#define MAN_GREEN_MODE						7
+#define CONFIRM_STATE						8
 
-extern int red_time;
-extern int green_time;
-extern int yellow_time;
+#define button_mode_is_pressed				10
+#define button_add_is_pressed				11
+#define button_confirm_is_pressed			12
+#define button_reset_is_pressed				13
 
-extern int PORTA;
-extern int status;
+#define button_mode_is_pressed_1s			14
+#define button_add_is_pressed_1s			15
+#define button_confirm_is_pressed_1s		16
+#define button_reset_is_pressed_1s			17
 
+#define INIT_TRAFFIC_LIGHT					19			// Initital state for traffic light 2-way
+
+#define RED_ROAD1							20			// RED ROAD 1 TURN
+#define	GREEN_ROAD1							21			// GREEN ROAD 1 TURN
+#define AMBER_ROAD1							22			// AMBER ROAD 1 TURN
+#define RED_ROAD2							23			// RED ROAD 2 TURN
+#define	GREEN_ROAD2							24			// GREEN ROAD 2 TURN
+#define AMBER_ROAD2							25			// AMBER ROAD 2 TURN
+
+
+extern int man_red_time;
+extern int man_green_time;
+extern int man_amber_time;
+extern int temp_value;
+
+extern int led_status1;
+extern int led_status2;
+extern int light_time;
+extern int light_time1;
+extern int status;	// 4 modes of traffic light such as: mode MODE, ADD, CONFIRM and RESET
+extern int index_led;
+
+extern int flagForButtonPressOneQuarterSecond[4];
+extern int AllowToExecuteAfterASecondPressed;
 #endif /* INC_GLOBAL_H_ */
--- source_code/Core/Src/global.c
+++ source_code/Core/Src/global.c
@@ -8,9 +8,18 @@
 #include "global.h"
 #include "main.h"
 
-int red_time = 500;
-int green_time = 300;
-int yellow_time = 200;
+// default light time for 3 states
+int man_red_time = 5;
+int man_amber_time = 2;
+int man_green_time = 3;
 
-int PORTA = 0;
-int status = 0;
+int temp_value = 0;
+int light_time = GREEN_TIME / 100;
+int light_time1 = RED_TIME / 100;
+int status = NORMAL_MODE;
+int led_status1 = INIT_TRAFFIC_LIGHT;
+int led_status2 = INIT_TRAFFIC_LIGHT;
+int index_led = 0;
+int AllowToExecuteAfterASecondPressed = 0;
+
+int flagForButtonPressOneQuarterSecond[4] = {0};
